e97e4a19660ef768f596dd79360f9f30aac2f131
santhoshreddynarra <santhoshreddynarra3029@gmail.com>
2026-07-19 22:59:58 +0530

    feat: implement login endpoint

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -35,4 +35,26 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { registerUser };
+// @desc    Auth user & get token
+// @route   POST /api/users/login
+// @access  Public
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
+});
+
+export { registerUser, authUser };
