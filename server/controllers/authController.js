7c582899de1fa23cfa6477f90e0683fa97f1252b
santhoshreddynarra <santhoshreddynarra3029@gmail.com>
2026-07-20 23:52:57 +0530

    fix: improve registration validation

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -8,6 +8,11 @@ import generateToken from '../utils/generateToken.js';
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('Please provide all required fields');
+  }
+
   const userExists = await User.findOne({ email });
 
   if (userExists) {
@@ -15,11 +20,17 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error('User already exists');
   }
 
-  const user = await User.create({
-    name,
-    email,
-    password,
-  });
+  let user;
+  try {
+    user = await User.create({
+      name,
+      email,
+      password,
+    });
+  } catch (error) {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
 
   if (user) {
     generateToken(res, user._id);
