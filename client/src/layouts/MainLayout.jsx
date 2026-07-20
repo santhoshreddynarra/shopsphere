db21b91965bd968f761c781c07a743e197a59a88
santhoshreddynarra <santhoshreddynarra3029@gmail.com>
2026-07-20 19:37:51 +0530

    feat: redesign global light theme

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -4,7 +4,7 @@ import Footer from '../components/Footer.jsx';
 
 const MainLayout = ({ children }) => {
   return (
-    <div className="min-h-screen flex flex-col bg-slate-950">
+    <div className="min-h-screen flex flex-col bg-slate-50 font-sans text-slate-900">
       <Navbar />
       <main className="flex-grow">{children}</main>
       <Footer />
