74b612baab79dc46f0fbf2ea95d6a3d060297f3d
santhoshreddynarra <santhoshreddynarra3029@gmail.com>
2026-07-20 23:11:38 +0530

    chore: remove seed endpoint

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -5,18 +5,8 @@ import {
   getProductBySlug,
   getRelatedProducts,
 } from '../controllers/productController.js';
-import { importData } from '../seed/seedCatalog.js';
 
 const router = express.Router();
-
-router.get('/seed', async (req, res) => {
-  try {
-    const result = await importData();
-    res.json(result);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
 
 router.get('/', getProducts);
 router.get('/featured', getFeaturedProducts);
