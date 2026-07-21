f825f6e01288a37c2214c7832eed2b142ce8c7b7
santhoshreddynarra <santhoshreddynarra3029@gmail.com>
2026-07-21 21:27:20 +0530

    debug api url

--- client/src/services/axiosInstance.js
+++ client/src/services/axiosInstance.js
@@ -1,21 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
+
+console.log("API URL =", import.meta.env.VITE_API_URL);
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || '/api',
+  baseURL: import.meta.env.VITE_API_URL || "/api",
   withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json',
-  },
 });
-
-// Response interceptor: handle global errors
-axiosInstance.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    const message =
-      error.response?.data?.message || error.message || 'Something went wrong';
-    return Promise.reject(new Error(message));
-  }
-);
 
 export default axiosInstance;
